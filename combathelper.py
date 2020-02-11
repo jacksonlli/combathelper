8d30647a204ba6ad7bf805a094f4d16a8ce52a9d
jacksonlli <59549394+jacksonlli@users.noreply.github.com>
2020-02-11 01:22:51 -0500

    added player api

--- combathelper.py
+++ combathelper.py
@@ -1,7 +1,26 @@
-
-from flask import Flask
+import mysql.connector
+from flask import Flask, request , jsonify, abort, make_response
 app = Flask(__name__)
 
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd="w4m973fy4u",
+  database="game1"
+)
+
 @app.route('/')
-def index():
+def hello():
     return "<h1> my backend!!! </h1>"
+
+# Get all players
+@app.route('/player', methods=['GET'])
+def get_players():
+    mycursor = mydb.cursor()
+    mycursor.execute("SELECT id, name FROM player")
+   # myresult = list(mycursor.fetchall())
+
+    myresult = [{'id':row[0], 'name':row[1]} for row in mycursor.fetchall()]
+
+
+    return jsonify(myresult)
